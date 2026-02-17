bc67847ef05c72fedf0ea114e4b72b127184bab2
Vishal Thakur <157201590+vishalthakur0012@users.noreply.github.com>
2026-02-17 06:25:48 -0500

    sdfjklsdjlfsd

--- resources/views/pages/listingPage/listing_grid_view.blade.php
+++ resources/views/pages/listingPage/listing_grid_view.blade.php
@@ -1,7 +1,7 @@
 <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
-    
+
 <section class="listing-area-front">
     <div class="container">
         <div class="row justify-content-center">
@@ -68,7 +68,12 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star">
                                             <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
                                         </svg>
-                                        <span>4.5</span>
+
+                                        @php
+                                        $avg = $listing->avg_rating ? number_format($listing->avg_rating, 1) : '0.0';
+                                        @endphp
+
+                                        <span>{{ $avg }}</span>
                                     </div>
 
                                     <div class="reviews">
@@ -78,7 +83,12 @@
                                             <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                                             <circle cx="9" cy="7" r="4" />
                                         </svg>
+
+                                        @if(($listing->ratings_count ?? 0) > 0)
+                                        <span>{{ $listing->ratings_count }} rating{{ $listing->ratings_count > 1 ? 's' : '' }}</span>
+                                        @else
                                         <span>No ratings</span>
+                                        @endif
                                     </div>
                                 </div>
 
@@ -87,6 +97,7 @@
                                     {{ $listing->categoryRel->name ?? '-' }}
                                 </div>
                             </div>
+
                         </div>
 
                         {{-- DESCRIPTION --}}
