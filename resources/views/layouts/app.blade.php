6c4fc05f73512406434a86da95f3a7cbb41a0a69
Vishal Thakur <157201590+vishalthakur0012@users.noreply.github.com>
2026-04-17 11:42:50 +0530

    sdklfnsldfsd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,10 +29,34 @@
     <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.jpg') }}">
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-TSSX88C6');
+    </script>
+    <!-- End Google Tag Manager -->
+
     @stack('styles')
 </head>
 
 <body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSSX88C6"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     {{-- 🔹 Navbar --}}
 
