e152e8a6b18ff6cb779d3811fead15388fddcd3b
manshu777 <79352371+Manshu777@users.noreply.github.com>
2026-05-29 17:26:21 +0530

    adfrwfrwvfwve

--- config/cors.php
+++ config/cors.php
@@ -12,9 +12,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-    ],
+    'allowed_origins' => array_values(array_filter(array_map(
+        'trim',
+        explode(',', env('CORS_ALLOWED_ORIGINS', implode(',', [
+            'http://localhost:3000',
+            'http://127.0.0.1:3000',
+            'https://citiinfo.com.au',
+            'https://www.citiinfo.com.au',
+            'https://crm.citiinfo.com.au',
+        ])))
+    ))),
 
     'allowed_origins_patterns' => [],
 
